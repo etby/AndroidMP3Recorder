b5b0a6dae940251ae4a32b80a09411c22fd5dd51
Etby Gao <etby.org@gmail.com>
2018-06-25 17:52:15 +0800

    增加Sample权限

--- sample/src/main/java/com/czt/mp3recorder/sample/MainActivity.java
+++ sample/src/main/java/com/czt/mp3recorder/sample/MainActivity.java
@@ -1,5 +1,9 @@
-
 package com.czt.mp3recorder.sample;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 
 import java.io.File;
 import java.io.IOException;
@@ -15,35 +19,66 @@ import com.czt.mp3recorder.MP3Recorder;
 
 public class MainActivity extends Activity {
 
-	private MP3Recorder mRecorder = new MP3Recorder(new File(Environment.getExternalStorageDirectory(),"test.mp3"));
+    public static final int REQUEST_CODE_WRITE_PERMISSION = 3;
+    private MP3Recorder mRecorder =
+            new MP3Recorder(new File(Environment.getExternalStorageDirectory(), "test_mp3.mp3"));
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		Button startButton = (Button) findViewById(R.id.StartButton);
-		startButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				try {
-					mRecorder.start();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		Button stopButton = (Button) findViewById(R.id.StopButton);
-		stopButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				mRecorder.stop();
-			}
-		});
-	}
+    private boolean mHasPermission = false;
 
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		mRecorder.stop();
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        Button startButton = (Button) findViewById(R.id.StartButton);
+        startButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!mHasPermission) {
+                    requestPermission();
+                    return;
+                }
+                try {
+                    mRecorder.start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        Button stopButton = (Button) findViewById(R.id.StopButton);
+        stopButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mRecorder.stop();
+            }
+        });
+        requestPermission();
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.RECORD_AUDIO
+                }, REQUEST_CODE_WRITE_PERMISSION);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_CODE_WRITE_PERMISSION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mHasPermission = true;
+                }
+                return;
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mRecorder.stop();
+    }
 }
